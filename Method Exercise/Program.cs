8cab40f9837e64daf56a0bcc851d72d5666ca4bf
Grant Hill <granthill@live.com>
2022-08-14 14:05:38 -0400

    Second commit.

--- Method Exercise/Program.cs
+++ Method Exercise/Program.cs
@@ -21,15 +21,13 @@ namespace Method_Exercise
             Console.WriteLine("Any favorite music? If so, what group?");
             String favoriteBand;
             favoriteBand= Console.ReadLine();
-            
+
             Console.WriteLine($"Once upon a time, there lived an individual named, {Name}, who was very jolly, happy go-lucky.");
             Console.WriteLine($"{Name}, had a young {favoriteAnimal}, which they looked after and took care of.... even took on exotic trips! Sometimes, they would even venture to live concerts together.");
             Console.WriteLine($"{Name}'s {favoriteAnimal} also happened to be a vivacious shade of {favoriteColor}.");
             Console.WriteLine($"Could you imagine a {favoriteColor} {favoriteAnimal}? I could! In fact I've seen dozens of them!");
             Console.WriteLine($"As it turned out, {Name}, and their beloved {favoriteAnimal} often attended {favoriteBand} concerts -- like you wouldn't believe.");
-            Console.WriteLine($"That {favoriteColor} {favoriteAnimal} went straight to the mash pit, without {Name}.");
-
-
+            Console.WriteLine($"That {favoriteColor} {favoriteAnimal} went straight to the mosh pit, without {Name}.");
         }
     }
 }
